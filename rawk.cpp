ec628ac8b20c408e4c797bf5d4030362d348c527
Robert Kooima <robert.kooima@gmail.com>
2014-06-21 10:16:55 -0500

    Add view registers

--- rawk.cpp
+++ rawk.cpp
@@ -8,6 +8,13 @@
 
 //------------------------------------------------------------------------------
 
+struct view
+{
+    double x;
+    double y;
+    double z;
+};
+
 class rawk : public gl::demonstration
 {
 public:
@@ -30,27 +37,23 @@ private:
     GLint  u_offset;
     GLint  u_scale;
 
-    int    point_x;             // Current mouse pointer position x
-    int    point_y;             // Current mouse pointer position y
+    int    selector;
+    bool   dragging;
 
-    double image_x;             // Current image view position x
-    double image_y;             // Current image view position y
-    double image_z;             // Current image view zoom
+    int    point_x;
+    int    point_y;
+    int    click_x;
+    int    click_y;
 
-    double cache_x;             // Cached image view position x
-    double cache_y;             // Cached image view position y
-    double cache_z;             // Cached image view zoom
-
-    bool   dragging;            // Is a drag in progress?
-    int    drag_point_x;        // Mouse pointer position at beginning of drag
-    int    drag_point_y;        // Mouse pointer position at beginning of drag
-    double drag_image_x;        // Image view position at beginning of drag
-    double drag_image_y;        // Image view position at beginning of drag
+    struct view store_view[12];
+    struct view image_view;
+    struct view cache_view;
+    struct view click_view;
 
     image *src;
 
     void refresh();
-    void  center();
+    void  center(struct view&);
 };
 
 rawk::rawk(image *src) : demonstration("RAWK", 1280, 720), src(src)
@@ -108,10 +111,14 @@ rawk::rawk(image *src) : demonstration("RAWK", 1280, 720), src(src)
     }
 
     dragging = false;
+    selector = 0;
     point_x  = 0;
     point_y  = 0;
 
-    center();
+    for (int i = 0; i < 12; i++)
+        center(store_view[i]);
+
+    center(image_view);
     refresh();
 }
 
@@ -130,27 +137,23 @@ static inline int toint(double d)
     return (c - d < d - f) ? int(c) : int(f);
 }
 
-void rawk::center()
+void rawk::center(struct view& v)
 {
     const int h = src->geth();
     const int w = src->getw();
 
     if (double(width) / double(height) < double(w) / double(h))
-        image_z = double(w) / double(width);
+        v.z = double(w) / double(width);
     else
-        image_z = double(h) / double(height);
+        v.z = double(h) / double(height);
 
-    image_x = w / 2;
-    image_y = h / 2;
+    v.x = w / 2;
+    v.y = h / 2;
 }
 
 void rawk::refresh()
 {
     std::vector<GLfloat> pixel(width * height * 3, 0);
-
-    cache_x = image_x;
-    cache_y = image_y;
-    cache_z = image_z;
 
     const int depth = std::min(src->getd(), 3);
 
@@ -158,22 +161,24 @@ void rawk::refresh()
         for     (int c = 0; c < width;  ++c)
             for (int k = 0; k < depth;  ++k)
             {
-                int i = toint(cache_y + (r - height / 2) * cache_z);
-                int j = toint(cache_x + (c - width  / 2) * cache_z);
+                int i = toint(image_view.y + (r - height / 2) * image_view.z);
+                int j = toint(image_view.x + (c - width  / 2) * image_view.z);
                 pixel[(r * width + c) * 3 + k] = src->get(i, j, k);
             }
 
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
                     GL_RGB, GL_FLOAT, &pixel.front());
+
+    cache_view = image_view;
 }
 
 void rawk::draw()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    double x = 2.0 * (cache_x - image_x) / cache_z / width;
-    double y = 2.0 * (cache_y - image_y) / cache_z / height;
-    double z =        cache_z / image_z;
+    double x = 2.0 * (cache_view.x - image_view.x) / cache_view.z / width;
+    double y = 2.0 * (cache_view.y - image_view.y) / cache_view.z / height;
+    double z =        cache_view.z / image_view.z;
 
     glUniform1f(u_scale,  z);
     glUniform2f(u_offset, x, y);
@@ -188,8 +193,8 @@ void rawk::motion(int x, int y)
 
     if (dragging)
     {
-        image_x = drag_image_x + (drag_point_x - x) * image_z;
-        image_y = drag_image_y + (drag_point_y - y) * image_z;
+        image_view.x = click_view.x + (click_x - x) * image_view.z;
+        image_view.y = click_view.y + (click_y - y) * image_view.z;
     }
 }
 
@@ -197,11 +202,10 @@ void rawk::button(int button, bool down)
 {
     if (button == SDL_BUTTON_LEFT)
     {
-        dragging     = down;
-        drag_image_x = image_x;
-        drag_image_y = image_y;
-        drag_point_x = point_x;
-        drag_point_y = point_y;
+        dragging   = down;
+        click_view = image_view;
+        click_x    = point_x;
+        click_y    = point_y;
     }
 }
 
@@ -214,32 +218,54 @@ void rawk::wheel(int dx, int dy)
 
     // Compute the image pixel on which the pointer lies.
 
-    double x = image_x + xx * image_z;
-    double y = image_y + yy * image_z;
+    double x = image_view.x + xx * image_view.z;
+    double y = image_view.y + yy * image_view.z;
 
     // Compute a new zoom level.
 
-    image_z = exp(log(image_z) - dy / 100.0);
+    image_view.z = exp(log(image_view.z) - dy / 100.0);
 
     // Compute image offsets to ensure the pointer remains over the same pixel.
 
-    image_x = x - xx * image_z;
-    image_y = y - yy * image_z;
+    image_view.x = x - xx * image_view.z;
+    image_view.y = y - yy * image_view.z;
 }
 
 void rawk::key(int key, bool down, bool repeat)
 {
-    if (down && !repeat)
+    if (!repeat)
     {
+        if (down)
+        {
+            switch (key)
+            {
+                case SDL_SCANCODE_SPACE:
+                    if (selector)
+                        image_view = store_view[selector - 1];
+                    refresh();
+                    break;
+
+                case SDL_SCANCODE_RETURN:
+                    if (selector)
+                        store_view[selector - 1] = image_view;
+                    break;
+            }
+        }
+
         switch (key)
         {
-            case SDL_SCANCODE_SPACE:
-                refresh();
-                break;
-
-            case SDL_SCANCODE_RETURN:
-                center();
-                break;
+            case SDL_SCANCODE_F1:  selector = down ?  1 : 0; break;
+            case SDL_SCANCODE_F2:  selector = down ?  2 : 0; break;
+            case SDL_SCANCODE_F3:  selector = down ?  3 : 0; break;
+            case SDL_SCANCODE_F4:  selector = down ?  4 : 0; break;
+            case SDL_SCANCODE_F5:  selector = down ?  5 : 0; break;
+            case SDL_SCANCODE_F6:  selector = down ?  6 : 0; break;
+            case SDL_SCANCODE_F7:  selector = down ?  7 : 0; break;
+            case SDL_SCANCODE_F8:  selector = down ?  8 : 0; break;
+            case SDL_SCANCODE_F9:  selector = down ?  9 : 0; break;
+            case SDL_SCANCODE_F10: selector = down ? 10 : 0; break;
+            case SDL_SCANCODE_F11: selector = down ? 11 : 0; break;
+            case SDL_SCANCODE_F12: selector = down ? 12 : 0; break;
         }
     }
 }
